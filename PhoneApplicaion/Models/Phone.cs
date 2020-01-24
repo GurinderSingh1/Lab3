3a957083d0105e468309bea09af79cb0d5fcee0f
Gurinder Singh <200424599@student.georgianc.on.ca>
2020-01-23 20:53:43 -0500

    Add project files.

---
+++ PhoneApplicaion/Models/Phone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhoneApplicaion.Models
+{
+    public class Phone
+    {
+      
+
+
+        public Phone() {
+            phoneId = 0 ;
+            phoneName = "";
+            manufacturer = "";
+            msrp = 0;
+            Screensize = 0;
+            DateReleased = DateTime.Now;
+
+        }
+        public string phoneName { get; set; }
+        public int phoneId { get; set; }
+        public  double  msrp { get; set; }
+        public double Screensize { get; set; }
+        public DateTime DateReleased { get; set; }
+        public string manufacturer { get; set; }
+ 
+   
+    }
+}
